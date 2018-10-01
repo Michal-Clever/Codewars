ea4a78e99f2c0a410821834a4a279bf2398316ec
Michal-Clever <inz.michal.milczarek@gmail.com>
2018-10-01 22:26:35 +0200

    Clock

---
+++ Clock.java
@@ -0,0 +1,13 @@
+public class Clock {
+
+
+    
+
+    public static int Past(int h, int m, int s)
+    {
+        //Happy Coding! ^_^
+        //  int h,m,s;
+        int sum = ( (h* 3600) + (m * 60 ) + (s ) );
+        return h=sum;
+    }
+}
