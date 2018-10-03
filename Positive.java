1de8c8779a7fe9cc3c652fa584b19da0c6cd9c47
Michal-Clever <inz.michal.milczarek@gmail.com>
2018-10-03 21:10:32 +0200

    Positive

---
+++ Positive.java
@@ -0,0 +1,12 @@
+public class Positive {
+    public static int sum(int[] arr){
+        int sum = 0;
+        for (int i : arr) {
+            if (i >0) {
+                sum=sum+1;
+            }
+        }
+        return sum;
+
+    }
+}
