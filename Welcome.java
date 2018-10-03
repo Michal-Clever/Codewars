c8d5b89c5b651807efde70b4cef85e18b3c7d7d3
Michal-Clever <inz.michal.milczarek@gmail.com>
2018-10-03 21:29:04 +0200

    Welcome

--- Welcome.java
+++ Welcome.java
@@ -1,6 +1,7 @@
 import java.util.HashMap;
 
 public class Welcome {
+
     // The Task
     // Think of a way to store the languages as a database (eg an object). The languages are listed below so you can copy and paste!
     // Write a 'welcome' function that takes a parameter 'language' (always a string), and returns a greeting - if you have it in your database.
