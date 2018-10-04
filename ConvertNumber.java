3521d5de948253074a6249fd38e96255e2b51bbb
Michal-Clever <inz.michal.milczarek@gmail.com>
2018-10-04 22:05:31 +0200

    ConvertNumber

--- ConvertNumber.java
+++ ConvertNumber.java
@@ -4,7 +4,6 @@ public class ConvertNumber {
         String numStr = String.valueOf(n);
         int digits[] = new int[numStr.length()];
         int counter = 0;
-
         for (int i = numStr.length() - 1; i >= 0; i--) {
             digits[i] = Integer.parseInt(Character.toString(numStr.charAt(counter++)));
         }
