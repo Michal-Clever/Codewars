51f131a406ff02fe35d1be95d724fb4db7ec62ac
Michal-Clever <inz.michal.milczarek@gmail.com>
2018-10-10 22:52:57 +0200

    Negative

---
+++ Negative.java
@@ -0,0 +1,12 @@
+import static java.lang.Math.abs;
+
+public class Negative {
+    public static void main(String[] args) {
+        int x = 5;
+        System.out.println(-abs(x));
+
+        System.out.println((x>0)?-x:x);
+
+
+    }
+}
