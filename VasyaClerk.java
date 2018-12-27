0e88d90ec0a77405bc16ce54c9330ecac284d750
Michal-Clever <inz.michal.milczarek@gmail.com>
2018-12-27 21:34:46 +0100

    VasyaClerk

--- VasyaClerk.java
+++ VasyaClerk.java
@@ -1,6 +1,26 @@
 public class VasyaClerk {
-
-
+    public static String Tickets(int[] peopleInLine) {
+        int bill25 = 0, bill50 = 0;
+        for (int payment : peopleInLine) {
+            if (payment == 25) {
+                bill25++;
+            } else if (payment == 50) {
+                bill25--;
+                bill50++;
+            } else if (payment == 100) {
+                if (bill50 > 0) {
+                    bill50--;
+                    bill25--;
+                } else {
+                    bill25 -= 3;
+                }
+            }
+            if (bill25 < 0 || bill50 < 0) {
+                return "NO";
+            }
+        }
+        return "YES";
+    }
 }
 //        The new "Avengers" movie has just been released! There are a lot of people at the cinema box office standing in a huge line.
 //        Each of them has a single 100, 50 or 25 dollars bill. An "Avengers" ticket costs 25 dollars.
