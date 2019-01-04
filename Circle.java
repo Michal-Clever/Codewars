e92ff617f00463689b05f48bff9a7f34c567544c
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-01-04 22:43:41 +0100

    Circle

--- Circle.java
+++ Circle.java
@@ -1,2 +1,14 @@
 public class Circle {
+
 }
+
+//         Complete the function circleArea so that it will return the area of a circle with the given radius.
+//        Round the returned number to two decimal places (except for Haskell).
+//        If the radius is not positive or not a number, return false.
+//
+//        Example:
+//
+//        Circle.area(-1485.86);    //returns false
+//        Circle.area(0);           //returns false
+//        Circle.area(43.2673);     //returns 5881.25
+//        Circle.area(68);          //returns 14526.72
