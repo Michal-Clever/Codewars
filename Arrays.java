16f00284f80e78c6a6e1fa4b09f4bf063b115cf9
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-01-09 08:22:29 +0100

    Arrays

--- Arrays.java
+++ Arrays.java
@@ -1,5 +1,7 @@
 public class Arrays {
+
     public static int findSmallest(final int[] numbers, final String toReturn) {
+
         int smallest = numbers[0];
         int index = 0;
         for (int i = 1; i < numbers.length; i++) {
