caddbd9aa535ab2d31d466cadf000feb099c75d7
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-01-13 17:45:04 +0100

    SequencesSum

--- SequenceSum.java
+++ SequenceSum.java
@@ -1,5 +1,19 @@
 public class SequenceSum{
+    public static int[] sumOfN(int n) {
+        int[] result = new int[Math.abs(n) + 1];
+        for (int i = 0; i < result.length; i++) {
+            int sum = 0;
+            for (int j = 0; j <=i; j++) {
+                sum += j;
+            }
 
+            if (n >= 0)
+                result[i] = sum;
+            else
+                result[i] = -sum;
+        }
+        return result;
+    }
 }
 
 //        Sum of 'n' Numbers
@@ -16,7 +30,9 @@ public class SequenceSum{
 //        0(0),1(0+1),3(0+1+2),6(0+1+2+3)...
 //        Pattern: "thenth term is the sum of numbers(from 0 ton, inclusive)"
 //
-//        sum_of_n (or SequenceSum.sumOfN in Java, SequenceSum.SumOfN in C#) takes an integer n and returns a List (an Array in Java/C#) of length abs(n) + 1. The List/Array contains the numbers in the arithmetic series produced by taking the sum of the consecutive integer numbers from 0 to n inclusive.
+//        sum_of_n (or SequenceSum.sumOfN in Java, SequenceSum.SumOfN in C#) takes an integer n and returns a List
+//        (an Array in Java/C#) of length abs(n) + 1. The List/Array contains the numbers in the arithmetic series produced
+//        by taking the sum of the consecutive integer numbers from 0 to n inclusive.
 //
 //        n can also be 0 or a negative value.
 //        Wikipedia reference for abs value is available here.
