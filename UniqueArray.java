824d7a0a208f90bf1f7a05a2b807e12b82922460
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-01-17 06:39:55 +0100

    UniqueArray

--- UniqueArray.java
+++ UniqueArray.java
@@ -1,3 +1,16 @@
-public class UniqueArray {
+import java.util.ArrayList;
 
+public class UniqueArray {
+    public static int[] unique(int[] integers) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i : integers) {
+            if(!list.contains(i)) {list.add(i);}
+        }
+        int[] out = new int[list.size()];
+        int count = 0;
+        for(Integer i : list) {
+            out[count++] = i;
+        }
+        return out;
+    }
 }
