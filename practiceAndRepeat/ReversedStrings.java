f6609091baefaf8ec73ef7d42cff1a3ca355da08
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-01-23 23:35:43 +0100

    ReversedStrings

--- practiceAndRepeat/ReversedStrings.java
+++ practiceAndRepeat/ReversedStrings.java
@@ -1,8 +1,12 @@
 package practiceAndRepeat;
 
 public class ReversedStrings {
-
+    public static String solution(String str) {
+        StringBuilder input = new StringBuilder();
+        input.append(str);
+        return input.reverse().toString();
+    }
 }
 
 
-    // Complete the solution so that it reverses the string value passed into it.
+// Complete the solution so that it reverses the string value passed into it.
