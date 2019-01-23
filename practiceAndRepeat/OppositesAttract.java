fd7e92b06976fbcfa091b84be38dcc687d91300a
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-01-23 23:00:21 +0100

    OppositesAttract

--- practiceAndRepeat/OppositesAttract.java
+++ practiceAndRepeat/OppositesAttract.java
@@ -1,5 +1,13 @@
 package practiceAndRepeat;
 
 public class OppositesAttract {
-
+    public static boolean isLove(final int x, final int y) {
+        return x % 2 != y % 2;
+    }
 }
+
+//            Timmy & Sarah think they are in love,
+//            but around where they live, they will only know once they pick a flower each.
+//                    If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+//
+//            Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
