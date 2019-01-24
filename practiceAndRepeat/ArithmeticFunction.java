f4e44eabf76546a5d1bf2b066886349b289b28f2
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-01-24 20:28:23 +0100

    ReversedStrings

---
+++ practiceAndRepeat/ArithmeticFunction.java
@@ -0,0 +1,26 @@
+package practiceAndRepeat;
+
+public class ArithmeticFunction {
+    public static int arithmetic(int a, int b, String operator) {
+        // your code here
+        int result;
+        switch (operator) {
+            case "add":
+                result = a + b;
+                break;
+            case "subtract":
+                result = a - b;
+                break;
+            case "multiply":
+                result = a * b;
+                break;
+            case "divide":
+                result = a / b;
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid argument: " + operator);
+        }
+
+        return result;
+    }
+}
