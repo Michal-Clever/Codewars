805da5b99bcd5b0fce04507bee6c453ba6b9e5d9
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-01-25 17:51:02 +0100

    Sequence

--- practiceAndRepeat/Sequence.java
+++ practiceAndRepeat/Sequence.java
@@ -1,7 +1,13 @@
 package practiceAndRepeat;
 
 public class Sequence {
-
+    public static int[] reverse(int n) {
+        //your code
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++)
+            res[i] = n - i;
+        return res;
+    }
 }
 
 
