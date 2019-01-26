5e4e79617b17aca99bfa3917676efbc05b487345
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-01-26 14:54:28 +0100

    Digitize

--- fundamentals/Digitize.java
+++ fundamentals/Digitize.java
@@ -3,3 +3,13 @@ package fundamentals;
 public class Digitize {
 
 }
+
+//    Convert number to reversed array of digits
+//        Given a random number:
+//
+//        C#: long;
+//        C++: unsigned long;
+//        You have to return the digits of this number within an array in reverse order.
+//
+//        Example:
+//        348597 => [7,9,5,8,4,3]
