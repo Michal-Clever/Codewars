8bb4e888e194997f18d569dd8aacb072b597f164
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-01-29 17:55:12 +0100

    DescendingOrder

--- fundamentals/DescendingOrder.java
+++ fundamentals/DescendingOrder.java
@@ -1,6 +1,23 @@
 package fundamentals;
 
-public class DescendingOrder {
+import java.util.Arrays;
+import java.util.Collections;
 
+public class DescendingOrder {
+    public static int sortDesc(final int num) {
+        String[] array = String.valueOf(num).split("");
+        Arrays.sort(array, Collections.reverseOrder());
+        return Integer.valueOf(String.join("", array));
+    }
 
 }
+
+//    Your task is to make a function that can take any non-negative integer as a argument and return it with its digits in descending order.
+//        Essentially, rearrange the digits to create the highest possible number.
+//
+//        Examples:
+//        Input: 21445 Output: 54421
+//
+//        Input: 145263 Output: 654321
+//
+//        Input: 1254859723 Output: 9875543221
