b34dc028f1eb1c6d1f2c37b3c290e9024c2b2ac6
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-01-30 19:52:18 +0100

    ReversedSequence

--- practiceAndRepeat/ReversedSequence.java
+++ practiceAndRepeat/ReversedSequence.java
@@ -2,7 +2,13 @@ package practiceAndRepeat;
 
 public class ReversedSequence {
 
-
+    public static int[] reverse(int n) {
+        //your code
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++)
+            res[i] = n - i;
+        return res;
+    }
 
 }
 //
