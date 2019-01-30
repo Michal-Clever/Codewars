6b5b14551b149f16c2d31059296a94b929492478
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-01-30 19:50:25 +0100

    ReversedSequence

--- practiceAndRepeat/ReversedSequence.java
+++ practiceAndRepeat/ReversedSequence.java
@@ -2,4 +2,10 @@ package practiceAndRepeat;
 
 public class ReversedSequence {
 
+
+
 }
+//
+//    Get the number n (n>0) to return the reversed sequence from n to 1.
+//
+//        Example : n=5 >> [5,4,3,2,1]
