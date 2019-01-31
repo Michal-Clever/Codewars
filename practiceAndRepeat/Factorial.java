e07b4ed02be01871c0c01d15b42be7c82a355e6d
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-01-31 17:33:53 +0100

    Factorial

--- practiceAndRepeat/Factorial.java
+++ practiceAndRepeat/Factorial.java
@@ -2,6 +2,18 @@ package practiceAndRepeat;
 
 public class Factorial {
 
+    public int factorial(int n) {
+        if (n < 0 || n > 12)
+            throw new IllegalArgumentException();
+
+        int result = 1;
+
+        for (int i = 2; i <= n; ++i) {
+            result *= i;
+        }
+
+        return result;
+    }
 
 }
 //
