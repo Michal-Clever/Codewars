3b4b1bef070cf99a78992c65de4618c08a236a5f
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-01-31 19:27:48 +0100

    MakeUpperCase

--- practiceAndRepeat/MakeUpperCase.java
+++ practiceAndRepeat/MakeUpperCase.java
@@ -1,5 +1,12 @@
 package practiceAndRepeat;
 
 public class MakeUpperCase {
-
+    public static String MakeUpperCase(String str){
+        return str.toUpperCase();
+    }
 }
+
+
+//    Write function makeUpperCase.
+
+
