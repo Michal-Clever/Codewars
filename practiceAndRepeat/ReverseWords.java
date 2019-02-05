fa0f20e3e480ba5058674702fb1c69e502f653b0
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-02-05 17:50:06 +0100

    ReverseWords

---
+++ practiceAndRepeat/ReverseWords.java
@@ -0,0 +1,26 @@
+package practiceAndRepeat;
+
+public class ReverseWords {
+    public static String reverseWords(final String original) {
+        String[] array = original.split(" ");
+
+        if (array.length == 0)
+            return original;
+
+
+        int i = 0;
+        for (String string : array) {
+            array[i] = new StringBuilder(string).reverse().toString();
+            i++;
+        }
+
+        return String.join(" ", array);
+    }
+}
+
+//    Complete the function that accepts a string parameter, and reverses each word in the string.
+//        All spaces in the string should be retained.
+//
+//        Examples
+//        "This is an example!" ==> "sihT si na !elpmaxe"
+//        "double  spaces"      ==> "elbuod  secaps"
