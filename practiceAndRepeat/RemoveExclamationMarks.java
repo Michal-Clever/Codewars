aa8b1c2c1e895b767ff2852cd783c67eec831cc7
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-02-07 17:27:04 +0100

    Playing with digits

---
+++ practiceAndRepeat/RemoveExclamationMarks.java
@@ -0,0 +1,9 @@
+package practiceAndRepeat;
+
+public class RemoveExclamationMarks {
+    static String removeExclamationMarks(String s) {
+        return s.replaceAll("!", "");
+    }
+
+}
+//Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
