a19f9ab8f6be4301f872fdc68db00119c7380956
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-02-25 18:31:52 +0100

    max diff easy

---
+++ fundamentals/Test.java
@@ -0,0 +1,19 @@
+package fundamentals;
+
+public class Test {
+    public static void main(String[] args) {
+
+
+        int x = 5;
+        int y = 7;
+        int k = -9;
+
+        int l = Math.max(x, y);
+        System.out.println(l);
+
+        int r = Math.abs(-7);
+        System.out.println(r);
+
+
+    }
+}
