442059d396dbc4cae932c5dea440bc0e7a4f344f
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-03-01 19:38:19 +0100

    Is It Negative Zero (-0)?

---
+++ fundamentals/IsItNegativeZero0.java
@@ -0,0 +1,15 @@
+package fundamentals;
+
+public class IsItNegativeZero0 {
+    public static Boolean isNegativeZero(Float n) {
+        return n.equals(-0f);
+    }
+}
+//
+//    There exist two zeroes: +0 (or just 0) and -0.
+//
+//        Write a function that returns true if the input number is -0 and false otherwise (True and False for Python).
+//
+//        In JavaScript / TypeScript / Coffeescript the input will be a number.
+//
+//        In Python / Java / C / Haskell the input will be a float.
