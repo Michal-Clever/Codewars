438785d24ea28140dd0a263ca4276b2650b3c624
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-03-22 00:11:14 +0100

    Principal Diagonal | VS | Secondary Diagonal

--- fundamentals/PrincipalDiagonalVsSecondaryDiagonal.java
+++ fundamentals/PrincipalDiagonalVsSecondaryDiagonal.java
@@ -1,4 +1,34 @@
 package fundamentals;
 
 public class PrincipalDiagonalVsSecondaryDiagonal {
+    public static String diagonal(int[][] matrix) {
+        int p = 0;
+        int d = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            p += matrix[i][i];
+            d += matrix[i][matrix.length - 1 - i];
+        }
+        return (p > d) ? "Principal Diagonal win!" : (d > p) ? "Secondary Diagonal win!" : "Draw!";
+    }
 }
+
+//    Principal Diagonal -- The principal diagonal in a matrix identifies those elements of the matrix running from North-West to South-East.
+//
+//        Secondary Diagonal -- the secondary diagonal of amatrix identifies those elements of the matrix running from North-East to South-West.
+//
+//        For example:
+//
+//        matrix:             [1, 2, 3]
+//        [4, 5, 6]
+//        [7, 8, 9]
+//
+//        principal diagonal: [1, 5, 9]
+//        secondary diagonal: [3, 5, 7]
+//        Task
+//        Your task is to find which diagonal is "larger": which diagonal has a bigger sum of their elements.
+//
+//        If the principal diagonal is larger, return "Principal Diagonal win!"
+//        If the secondary diagonal is larger, return "Secondary Diagonal win!"
+//        If they are equal, return "Draw!"
+//        Note: You will always receive matrices of the same dimension.
+//
