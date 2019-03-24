96a52d6090c4d3289a43561a0eb337fa518880e3
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-03-24 22:19:52 +0100

    Recursive reverse string

---
+++ fundamentals/RecursiveReverseString.java
@@ -0,0 +1,31 @@
+package fundamentals;
+
+public class RecursiveReverseString {
+    public String reverse(String str) {
+        if (str == null || str.length() <= 1) {
+            return str;
+        }
+
+        return reverse(str.substring(1)) + str.charAt(0);
+    }
+}
+//
+//        Classic factorial counting on Javascript
+//        function factorial(n) {
+//        return n <= 1 ? 1 : n * factorial(n-1)
+//        }
+//        Your objective is to complete a recursive function reverse() that receives str as String and returns the same string in reverse order
+//
+//        Rules:
+//
+//        reverse function should be executed exactly N times. N = length of the input string
+//        helper functions are not allowed
+//        changing the signature of the function is not allowed
+//        Examples:
+//
+//        reverse("hello world") = "dlrow olleh" (N = 11)
+//        reverse("abcd") = "dcba" (N = 4)
+//        reverse("12345") = "54321" (N = 5)
+//        All tests for this Kata are randomly generated, besides checking the reverse logic they will count how many times the reverse()
+//        function has been executed.
+
