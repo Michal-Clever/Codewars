870b5253472c4a20daf758aa5e5d4cbc32b49c77
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-04-03 22:16:52 +0200

    Complete The Pattern 4

---
+++ fundamentals/CompleteThePattern4.java
@@ -0,0 +1,37 @@
+package fundamentals;
+
+public class CompleteThePattern4 {
+    public static String pattern(int n) {
+        final StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j <= n; j++) sb.append(j);
+            sb.append("\n");
+        }
+        return sb.toString().trim();
+    }
+}
+
+//        ##Task:
+//
+//        You have to write a function pattern which creates the following pattern upto n number of rows.
+//                If the Argument is 0 or a Negative Integer then it should return "" i.e. empty string.
+//
+//        ##Examples:
+//
+//        pattern(4):
+//
+//        1234
+//        234
+//        34
+//        4
+//        pattern(6):
+//
+//        123456
+//        23456
+//        3456
+//        456
+//        56
+//        6
+//        Note: There are no blank spaces
+//
+//        Hint: Use \n in string to jump to next line
