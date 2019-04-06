87b96a46acfbac96d5430aba596832ad938dbc0d
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-04-06 07:53:17 +0200

    ToLeetSpeak

---
+++ fundamentals/ToLeetSpeak.java
@@ -0,0 +1,36 @@
+package fundamentals;
+
+public class ToLeetSpeak {
+    static String toLeetSpeak(final String speak) {
+        StringBuilder sb = new StringBuilder(speak.length());
+        for (char c: speak.toCharArray()) {
+            switch (c) {
+                case 'A': sb.append('@'); break;
+                case 'B': sb.append('8'); break;
+                case 'C': sb.append('('); break;
+                case 'E': sb.append('3'); break;
+                case 'G': sb.append('6'); break;
+                case 'H': sb.append('#'); break;
+                case 'I': sb.append('!'); break;
+                case 'L': sb.append('1'); break;
+                case 'O': sb.append('0'); break;
+                case 'S': sb.append('$'); break;
+                case 'T': sb.append('7'); break;
+                case 'Z': sb.append('2'); break;
+                default: sb.append(c); break;
+            }
+        }
+        return sb.toString();
+    }
+}
+//
+//    Your task is to write a function toLeetSpeak that converts a regular english sentence to Leetspeak.
+//
+//        More about LeetSpeak You can read at wiki -> https://en.wikipedia.org/wiki/Leet
+//
+//        Consider only uppercase letters (no lowercase letters, no numbers) and spaces.
+//
+//        For example:
+//
+//        toLeetSpeak("LEET") returns "1337"
+//        In this kata we use a simple LeetSpeak dialect. Use this alphabet:
