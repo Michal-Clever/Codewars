95605b7932ca3e961c5f62b89c8739249abc1209
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-04-29 23:30:04 +0200

    The Deaf Rats Of Hamelin

---
+++ fundamentals/TheDeafRatsOfHamelin.java
@@ -0,0 +1,27 @@
+package fundamentals;
+
+public class TheDeafRatsOfHamelin {
+    public static int countDeafRats(final String town) {
+        String t = town.replaceAll(" ", "");
+        int count = 0;
+        for (int i = 0; i < t.length(); i += 2) if (t.charAt(i) == 'O') count++;
+        return count;
+    }
+}
+
+//        Story
+//        The Pied Piper has been enlisted to play his magical tune and coax all the rats out of town.
+//
+//        But some of the rats are deaf and are going the wrong way!
+//
+//        Kata Task
+//        How many deaf rats are there?
+//
+//        Legend
+//        P = The Pied Piper
+//        O~ = Rat going left
+//        ~O = Rat going right
+//        Example
+//        ex1 ~O~O~O~O P has 0 deaf rats
+//        ex2 P O~ O~ ~O O~ has 1 deaf rat
+//        ex3 ~O~O~O~OP~O~OO~ has 2 deaf rats
