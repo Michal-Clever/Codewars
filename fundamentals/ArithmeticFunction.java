649062c9bc1c46c3311ff2453a51d9bc8c56908d
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-06-11 23:53:54 +0200

    ArithmeticFunction

--- fundamentals/ArithmeticFunction.java
+++ fundamentals/ArithmeticFunction.java
@@ -2,6 +2,7 @@ package fundamentals;
 
 public class ArithmeticFunction {
     public static int arithmetic(int a, int b, String operator) {
+
         switch(operator) {
             case "add":
                 return a + b;
